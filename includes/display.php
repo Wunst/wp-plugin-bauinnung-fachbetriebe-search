fd811137f648d6826d965db11ce34913eaff0d94
Wunst <matthiesbe@gmail.com>
2024-12-27 11:36:56 +0100

    Setup JS environment for block

--- includes/display.php
+++ includes/display.php
@@ -2,21 +2,6 @@
 add_action( "init", "fachb_register_block" );
 
 function fachb_register_block() {
-  wp_register_script(
-    "bauinnung-kiel-fachb-block-editor",
-    fachb_PLUGURL . "/display/index.js",
-    array(
-      "wp-block-editor",
-      "wp-blocks",
-      "wp-element"
-    ),
-    filemtime( fachb_PLUGDIR . "display/index.js" )
-  );
-  register_block_type(
-    fachb_PLUGDIR . "/display",
-    array(
-      "editor_script" => "bauinnung-kiel-fachb-block-editor"
-    )
-  );
+  register_block_type( fachb_PLUGDIR . "/build" );
 }
 

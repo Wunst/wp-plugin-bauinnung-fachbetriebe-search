bcbc6b1043c14a8d15d00ab36e9de2f5673689a1
Wunst <matthiesbe@gmail.com>
2025-01-03 18:27:42 +0100

    Geocoding/sorting functionality

---
+++ includes/geo.php
@@ -0,0 +1,58 @@
+<?php
+
+use Geocoder\Query\GeocodeQuery;
+
+$provider = new \Geocoder\Provider\Cache\ProviderCache(
+  \Geocoder\Provider\Nominatim\Nominatim::withOpenStreetMapServer(
+    \Symfony\Component\HttpClient\HttpClient::create(),
+    "Fachbetriebesuche der Bauinnung Kiel"
+  ),
+  new \Symfony\Component\Cache\Adapter\FilesystemAdapter()
+);
+
+$geocoder = new \Geocoder\StatefulGeocoder( $provider, "de" );
+
+/*
+ * Ermittelt Distanz zwischen Koordinaten.
+ */
+function fachb_haversine( $a, $b ) {
+    $earthRadius = 6371; // Earth's radius in kilometers
+
+    $latFrom = deg2rad( $a->getLatitude() );
+    $lonFrom = deg2rad( $a->getLongitude() );
+    $latTo = deg2rad( $b->getLatitude() );
+    $lonTo = deg2rad( $b->getLongitude() );
+
+    $latDelta = $latTo - $latFrom;
+    $lonDelta = $lonTo - $lonFrom;
+
+    $angle = 2 * asin(sqrt(pow(sin($latDelta / 2), 2) +
+        cos($latFrom) * cos($latTo) * pow(sin($lonDelta / 2), 2)));
+    return $angle * $earthRadius;
+}
+
+/*
+ * Ermittelt die Distanz in Kilometern zwischen addressA und addressB.
+ */
+function fachb_distance( $addressA, $addressB ) {
+  $a = $geocoder->geocode(
+    GeocodeQuery::create( $addressA ) 
+  )->first();
+
+  $b = $geocoder->geocode(
+    GeocodeQuery::create( $addressB ) 
+  )->first();
+
+  if ( !$a || !$b ) {
+    return null;
+  }
+
+  return fachb_haversine( $a->getCoordinates(), $b->getCoordinates() );
+}
+
+function fachb_check_address( $address ) {
+  return null != $geocoder->geocode(
+    GeocodeQuery::create( $address )
+  )->first();
+}
+
---
+++ includes/rest.php
@@ -0,0 +1,62 @@
+<?php
+
+require_once( fachb_PLUGDIR . "includes/db.php" );
+require_once( fachb_PLUGDIR . "includes/geo.php" );
+
+add_action( "rest_api_init", "fachb_rest_api_init" );
+
+function fachb_rest_api_init() {
+  register_rest_route( "fachbetrieb/v1", "/search", array(
+    "methods" => "GET",
+    "callback" => "fachb_rest_search"
+  ) );
+}
+
+/*
+ * GET /fachbetrieb/v1/search?a=[adresse]&d=[maxDistanz]
+ * Liefert die Fachbetriebe nach Entfernung von der Adresse geordnet.
+ *
+ * Response:
+ *  {
+ *    "sorted": "boolean",
+ *    "results": [
+ *      {
+ *        "name": "string",
+ *        "adresse": "string",
+ *        "url": "string?",
+ *        "distance": "number?"
+ *      }
+ *    ]
+ *  }
+ *
+ * Anmerkung: sorted ist false, wenn die Adresse des Kunden ("a=") von
+ * Nominatim nicht aufgelöst werden konnte.
+ */
+function fachb_rest_search( WP_REST_Request $request ) {
+  // TODO: filtern nach Kategorie.
+  $betriebe = fachb_list();
+
+  $address = $request[ "a" ];
+  if ( !$address || !fachb_check_address( $address ) ) {
+    // Keine Kundenadresse oder Kundenadresse ungültig.
+    return array(
+      "sorted" => false,
+      "results" => $betriebe // Betriebe in Reihenfolge des Hinzufügens.
+    );
+  }
+
+  // Betrieben Entfernung zuordnen.
+  $betriebe = array_map( function ( $b ) use ( $address ) {
+    return array_merge( $b, array(
+      "distance" => fachb_distance( $address, $b->adresse );
+  }, $betriebe );
+
+  return array(
+    "sorted" => true,
+    "results" => usort( $betriebe, function ( $a, $b ) {
+      // FIXME: was wenn Betrieb ungültige Adresse hat? (ganz hinten einsortieren?)
+      return $a->distance <=> $b->distance;
+    } )
+  );
+}
+

8c12f3e266add9d1b983f38fc99f8006e6fe5f98
Ben Matthies <ben@matthsch.de>
2026-07-23 12:09:47 +0200

    Simpler REST API
    The new REST API only contains *one* method for resolving an address.
    Client will be served coordinates of companies and user, and
    is expected to do distance calculations itself.

--- includes/rest.php
+++ includes/rest.php
@@ -1,129 +1,41 @@
 <?php
 
-require_once( fachb_PLUGDIR . "includes/db.php" );
-require_once( fachb_PLUGDIR . "includes/geo.php" );
 
-add_action( "rest_api_init", "fachb_rest_api_init" );
+namespace Fachbetrieb\RestApi;
 
-function fachb_rest_api_init() {
-  register_rest_route( "fachbetrieb/v1", "/search", array(
-    "methods" => "GET",
-    "callback" => "fachb_rest_search"
-  ) );
+require_once( FACHBETRIEB_PLUGDIR . "includes/db.php" );
+require_once( FACHBETRIEB_PLUGDIR . "includes/geo.php" );
 
-  register_rest_route( "fachbetrieb/v1", "/categories", array(
-    "methods" => "GET",
-    "callback" => "fachb_rest_categories"
-  ) );
 
-  register_rest_route( "fachbetrieb/v1", "/betrieb/categories", array(
-    "methods" => "GET",
-    "callback" => "fachb_rest_betrieb_categories"
-  ) );
-}
+use \Fachbetrieb\Geo;
 
-/*
- * GET /fachbetrieb/v1/search?c=[kategorien]&a=[adresse]&d=[maxDistanz]
- * Liefert die Fachbetriebe nach Entfernung von der Adresse geordnet.
- *
- * c= Komma-getrennte Liste von Kategorien
- * Betrieb muss ALLE Kategorien haben, um angezeigt zu werden.
- * a= Hausnummer,Straße,PLZ,Ort
- * d= Dezimalwert max. Distanz in km
- *
- * Response:
- *  {
- *    "sorted": "boolean",
- *    "results": [
- *      {
- *        "name": "string",
- *        "adresse": "string",
- *        "url": "string?",
- *        "distance": "number?"
- *      }
- *    ]
- *  }
- *
- * Anmerkung: sorted ist false, wenn die Adresse des Kunden ("a=") von
- * Nominatim nicht aufgelöst werden konnte.
- */
-function fachb_rest_search( WP_REST_Request $request ) {
-  $cat = $request[ "c" ];
-  $betriebe = $cat ?
-    fachb_list_with_categories(
-      array_map( "intval", explode( ",", $request[ "c" ] ) ) 
-    ) :
-    fachb_list();
 
-  $address = $request[ "a" ];
-  if ( !$address || !fachb_check_address( $address ) ) {
-    // Keine Kundenadresse oder Kundenadresse ungültig.
-    return array(
-      "sorted" => false,
-      "results" => $betriebe // Betriebe in Reihenfolge des Hinzufügens.
-    );
-  }
+add_action( "rest_api_init", \Fachbetrieb\RestApi\init(...) );
 
-  // Betrieben Entfernung zuordnen.
-  $betriebe = array_map( function ( $b ) use ( $address ) {
-    return array_merge( (array)$b, array(
-      "distance" => fachb_distance( $address, $b->adresse )
-    ) );
-  }, $betriebe );
-
-  // Betriebe nach Entfernung filtern.
-  $d = $request[ "d" ];
-  if ( $d ) {
-    $betriebe = array_filter( $betriebe, function ( $b ) use ( $d ) {
-      return floatval( $b["distance"] ) <= floatval( $d );
-    } );
-  }
-
-  // Betriebe nach Entfernung sortieren.
-  usort( $betriebe, function ( $a, $b ) {
-    // FIXME: was wenn Betrieb ungültige Adresse hat? (ganz hinten einsortieren?)
-    return $a["distance"] <=> $b["distance"];
-  } );
-
-  return array(
-    "sorted" => true,
-    "results" => $betriebe
+function init( ): void {
+  register_rest_route( 
+    'fachbetrieb/v1', 
+    '/coordinates', 
+    array(
+      'methods' => 'GET',
+      'callback' => \Fachbetrieb\RestApi\coordinates(...),
+    )
   );
 }
 
-/*
- * GET /fachbetrieb/v1/categories
- * Liefert alle Kategorien zurück.
- *
- * Response:
- *  [
- *    {
- *      "id": "number",
- *      "name": "string"
- *    }
- *  ]
+/**
+ * Resolve address to coordinates.
+ * @param \WP_REST_Request $request  Request URL
+ * should be of the form `/coordinates?address=...`
+ * @return array[
+ *   'lat' => float,
+ *   'lon' => float,
+ * ]  Coordinates, or null if not resolved.
  */
-function fachb_rest_categories( WP_REST_Request $request ) {
-  return fachb_category_list();
-}
-
-/*
- * GET /fachbetrieb/v1/betrieb/categories?id=[id]
- * Liefert die Kategorien für einen Betrieb.
- *
- * Response:
- *  [
- *    {
- *      "id": "number",
- *      "name": "string"
- *    }
- *  ]
- */
-function fachb_rest_betrieb_categories( WP_REST_Request $request ) {
-  if ( !$request["id"] ) {
-    return null;
-  }
-
-  return fachb_get_categories( intval( $request["id"] ) );
+function coordinates( \WP_REST_Request $request ): array {
+  /// XXX: This may require rate limiting.
+  // We'll see. If the server gets ddosed one 
+  // day this is it.
+  return Geo\resolve( $request['address'] );
 }
 
